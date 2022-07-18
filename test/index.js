d28522da82b7f24283cb87503adb6c62d82452cb
Patricia <waltraute@hotmail.com>
2022-07-18 18:59:14 +0200

    connected DB

--- test/index.js
+++ test/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/curs_nodejs', (err, res) => {
+    if (err) {
+        throw err
+    } else {
+       console.log('Connected to database')}
+    })
+
+
+
+
+
+
+
+
+
+
+
+
