897ba9466fa9ed1d67c34ab03d972ad63f7342d9
Patricia <waltraute@hotmail.com>
2022-07-19 18:10:59 +0200

    crear model imatge

---
+++ test/models/imatge.js
@@ -0,0 +1,11 @@
+'use strict';// per emprar expressions de js més modernes
+
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+let ImatgeSchema = Schema({
+    arxiu: String,
+    usuari : { type: Schema.ObjectId, ref: 'Usuari' }
+})
+
+module.exports = mongoose.model('Imatge', ImatgeSchema)
