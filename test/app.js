f2486ee202240d2684e3d110df9bfe39bea1e6cd
Patricia <waltraute@hotmail.com>
2022-07-19 17:33:22 +0200

    fer funcions test a middles

--- test/app.js
+++ test/app.js
@@ -6,17 +6,34 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const funcioTest = () => {
+    console.log('funcioTest');
+}
+
+const funcioTest2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve(true)
+        }, 3000)
+        })}
+
 //middle1
 app.use((req, res, next) => {
+    setTimeout(() => {
     console.log('Middleware 1')
+    funcioTest()
     next()
+    }, 2000)
+   
+
 })
 
 //middle2
-app.use((req, res, next) => {
-    console.log('Middleware 2')
-    next()
-})
+app.use(async (req, res, next) => {
+    const resProm = await funcioTest2()
+    console.log('Middleware 2, resProm: ', resProm) // el resProm es true
+     next()
+   })
 
 
 app.get('/get', (req, res) => {
