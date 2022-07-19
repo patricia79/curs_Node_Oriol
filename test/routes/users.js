c32f878271b08ddb0aee7bec3264deb131479a61
Patricia <waltraute@hotmail.com>
2022-07-19 17:44:17 +0200

    fer arxiu rutes

---
+++ test/routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const api = express.Router()
+
+
+api.get('/get', (req, res) => {
+    res.send('Sóc un get')
+})
+
+
+api.post('/post', (req, res) => {
+    res.json(req.body)// et torna un json amb les dades. al postman vas al body, form-encode i poses els camps/dades amb els valors que vulguis
+})
+
+
+api.put('/put', (req, res) => {
+    res.send('Sóc un put')
+})
+
+
+api.delete('/delete', (req, res) => {
+    res.send('Sóc un delete')
+})
+
+module.exports = api
