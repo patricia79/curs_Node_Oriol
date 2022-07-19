4c3479fac3945d05229b4e4b7b7411809791863d
Patricia <waltraute@hotmail.com>
2022-07-19 19:05:36 +0200

    crear ruta crear usuari

--- test/routes/users.js
+++ test/routes/users.js
@@ -6,7 +6,7 @@ let UsuariController = require("../controllers/usuari");
 const api = express.Router();
 
 api.get("/tots", UsuariController.proves);
-
+api.post("/registre", UsuariController.guardarUsuari);
 /*
 api.post("/post", (req, res) => {
   res.json(req.body); // et torna un json amb les dades. al postman vas al body, form-encode i poses els camps/dades amb els valors que vulguis
