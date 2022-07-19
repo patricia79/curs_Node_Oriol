0de2326778519987625368473c18b234664715dd
Patricia <waltraute@hotmail.com>
2022-07-19 17:44:36 +0200

    modificar arxiu amb rutes

--- test/app.js
+++ test/app.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+/*
 const funcioTest = () => {
     console.log('funcioTest');
 }
@@ -34,28 +35,6 @@ app.use(async (req, res, next) => {
     console.log('Middleware 2, resProm: ', resProm) // el resProm es true
      next()
    })
-
-
-app.get('/get', (req, res) => {
-    res.send('Sóc un get')
-})
-
-
-app.post('/post', (req, res) => {
-    res.json(req.body)// et torna un json amb les dades. al postman vas al body, form-encode i poses els camps/dades amb els valors que vulguis
-})
-
-
-app.put('/put', (req, res) => {
-    res.send('Sóc un put')
-})
-
-
-app.delete('/delete', (req, res) => {
-    res.send('Sóc un delete')
-})
-
-
-
+ */
 
 module.exports = app
