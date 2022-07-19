0fe5ce6af19f25b5232a629cc884b57ad43b7e02
Patricia <waltraute@hotmail.com>
2022-07-19 16:11:08 +0200

    updated index

--- test/index.js
+++ test/index.js
@@ -1,21 +1,19 @@
 let mongoose = require('mongoose')
-const express = require('express')
+const app = require('./app')
+const port = 3000
+
+
 
 
 mongoose.connect('mongodb://localhost:27017/curs_nodejs', (err, res) => {
     if (err) {
         throw err
     } else {
+        app.listen(port, ()=>{
+            console.log(`Server's exemple`);
+        })
        console.log('Connected to database')}
     })
 
-    const app = express()
-    const port = 3000
-
-    app.get('/', (req, res) => {
-        res.send('Hello girls')
-    })
-
-    app.listen(port, ()=>{
-        console.log(`Server's exemple`);
-    })
+    
+    
