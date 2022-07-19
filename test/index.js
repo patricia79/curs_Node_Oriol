a0081cba04df129f687455e5d80aec7cd8f076cb
Patricia <waltraute@hotmail.com>
2022-07-19 15:32:22 +0200

    server ok

--- test/index.js
+++ test/index.js
@@ -6,6 +6,19 @@ mongoose.connect('mongodb://localhost:27017/curs_nodejs', (err, res) => {
        console.log('Connected to database')}
     })
 
+    const http = require('http')
+    const hostname = '127.0.0.1'
+    const port = 3000
+    const server = http.createServer((req, res) => { // tot això ho emmagatzemo a la cte server
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')// a la capçaleera de la petició un texte pla
+        res.end('Hello World\n')// contesto un string
+    })
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`)
+    }
+    
+
 
 
 
