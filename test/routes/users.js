348d5b8948b69e254275183e9d55d665c92b920a
Patricia <waltraute@hotmail.com>
2022-07-20 16:22:37 +0200

    ruta veure usuari creada

--- test/routes/users.js
+++ test/routes/users.js
@@ -6,6 +6,7 @@ let UsuariController = require("../controllers/usuari");
 const api = express.Router();
 
 api.post("/registre", UsuariController.guardarUsuari);
+api.post("/veureUsuari", UsuariController.veureUsuari);
 /*
 api.post("/post", (req, res) => {
   res.json(req.body); // et torna un json amb les dades. al postman vas al body, form-encode i poses els camps/dades amb els valors que vulguis
