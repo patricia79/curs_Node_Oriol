84daca8846557e5f148db06472e5fa4d69052bfd
Patricia <waltraute@hotmail.com>
2022-07-20 16:16:49 +0200

    veure Usuari fet

--- test/controllers/usuari.js
+++ test/controllers/usuari.js
@@ -33,11 +33,11 @@ function guardarUsuari(req, res) {
   }
 }
 
-function veureUsuari(req, res) {
+function veureUsuari(req, res) { // veure un usuari concret, però fem un post pq ficarem un mail en el body
   let params = req.body;
-  let email = params.email; // recollim el email que arriba en la peticio amb el metode post
+  let email = params.email.toLowerCase; // recollim el email que arriba en la peticio amb el metode post
 
-  Usuari.findOne({ email: email.toLowerCase() }, (err, usuari) => {
+  Usuari.findOne({ email: email }, (err, usuari) => {
     //permet cercar un registre per una propietat i hem de definir-li una arrow function amb l'error i l'objecte
     if (err) {
       res.status(500).send({ message: "Error en la sol·licitud" });
@@ -45,6 +45,7 @@ function veureUsuari(req, res) {
       if (!usuari) {
         res.status(404).send({ message: "Les credencials no son correctes" });
       } else {
+        // dades de l'usuari loguejat       
         res.status(200).send({ usuari }); //si no indiquem cap propietat, emprarà la de l'usuari que coincideix amb el nom de l'objecte
       }
     }
